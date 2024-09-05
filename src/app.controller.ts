95d58174e13425ffda9452b9ea4c88ba4cfa1e7f
Felipe <felipe.tiburcio@gmail.com>
2024-09-04 22:02:17 -0300

    post method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { BooksService } from './app.service';
 import { Book } from './FakeDatabase';
 
@@ -8,11 +8,20 @@ export class BooksController {
 
   @Get()
   getAllBooks(): Book[] {
-    return this.booksService.getAllBooks();
+    return this.booksService.getAll();
   }
 
   @Get(':id')
   getBookById(@Param('id') id: string): Book | undefined {
     return this.booksService.findById(Number(id));
+  }
+
+  @Post()
+  postBook(@Body() book: Partial<Book>): Book | undefined {
+    if (!book.author || !book.title || !book.publicationYear) {
+      return undefined;
+    }
+
+    return this.booksService.save(book);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,12 +3,27 @@ import { Book, books } from './FakeDatabase';
 
 @Injectable()
 export class BooksService {
-  getAllBooks(): Book[] {
+  getAll(): Book[] {
     return books;
   }
 
   findById(id: number): Book | undefined {
     const book = books.find((book) => book.id === id);
     return book;
+  }
+
+  save(book: Partial<Book>): Book | undefined {
+    const newId = books[books.length - 1].id + 1;
+
+    const newBook = {
+      id: newId,
+      title: book.title,
+      author: book.author,
+      publicationYear: book.publicationYear,
+    };
+
+    books.push(newBook);
+
+    return newBook;
   }
 }
